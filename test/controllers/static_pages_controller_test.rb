e1ed787b7eb25028b737613433b26f8772dde9b2
donte <digitalartcrew@gmail.com>
2015-08-06 20:02:49 -0700

    Finish Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | The Muse"
   end
 
   test "should get events" do
     get :events
     assert_response :success
+    assert_select "title", "Events | The Muse"
   end
 
    test "should get recipes" do
     get :recipes
     assert_response :success
+    assert_select "title", "Recipes | The Muse"
   end
 
    test "should get rooms" do
     get :rooms
     assert_response :success
+    assert_select "title", "Rooms | The Muse"
   end
 
    test "should get payments" do
     get :payments
     assert_response :success
+    assert_select "title", "Payments | The Muse"
   end
 
 end
